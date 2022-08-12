d37f90e339d7a53b8985a9ae610117057486e4e9
Marcello <98602240+Tockawa@users.noreply.github.com>
2022-08-12 13:07:40 -0300

    sum more formatting

--- index.ts
+++ index.ts
@@ -94,4 +94,37 @@ const formatEverythingDown = replaceEverything
         "Não clico em links aleatórios, ou entro em sites desconhecidos."
     )
     .replaceAll("..", ".");
-console.log(formatEverythingDown);
+
+//count how many times the phrase "Não clico em links aleatórios ou entro em sites desconhecidos." appears in the string
+const matchMany = formatEverythingDown.match(
+        /Não clico em links aleatórios, ou entro em sites desconhecidos./g
+    ),
+    matchMany2 = formatEverythingDown.match(
+        /Deixo apenas meu antivirus tomar conta de meu computador./g
+    ),
+    matchMany3 = formatEverythingDown.match(
+        /Ignoro e-mails que caem diretamente na caixa de spam./g
+    ),
+    matchMany4 = formatEverythingDown.match(
+        /Mantenho meu windows\/linux\/macOS com as últimas atualizações./g
+    ),
+    matchMany5 = formatEverythingDown.match(
+        /Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive./g
+    ),
+    matchMany6 = formatEverythingDown.match(
+        /Atualizo meu navegador sempre que uma atualização nova é lançada./g
+    ),
+    matchMany7 = formatEverythingDown.match(
+        /Não tomo nenhuma dessas precauções/g
+    );
+const result = {
+    "Não tomo nenhuma dessas precauções": matchMany7!.length,
+    "Não clico em links aleatórios, ou entro em sites desconhecidos.": matchMany!.length,
+    "Deixo apenas meu antivirus tomar conta de meu computador.": matchMany2!.length,
+    "Ignoro e-mails que caem diretamente na caixa de spam.": matchMany3!.length,
+    "Mantenho meu windows/linux/macOS com as últimas atualizações.": matchMany4!.length,
+    "Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive.": matchMany5!.length,
+    "Atualizo meu navegador sempre que uma atualização nova é lançada.": matchMany6!.length,
+}
+
+console.log(formatEverythingDown + "\n\n\n" + JSON.stringify(result, null, 4));
