33b8760b710689c94ae124f826b8dfd06d6f6ab8
Marcello <98602240+Tockawa@users.noreply.github.com>
2022-08-12 12:36:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This was made for formatting a simple text, just thought it'd be cool to upload it and show to my teachers (Hello you ;) )
+Isso aqui foi feito apenas para formatar um texto simples de um formulário do google, na opção de múltiplas escolhas. Pensei que seria legal compartilhar este arquivo com meus professores (fala tu) 
---
+++ index.ts
@@ -0,0 +1,97 @@
+const everything: string = `
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador, Não tomo nenhuma dessas precauções
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Mantenho meu windows/linux/macOS com as últimas atualizações
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Mantenho meu windows/linux/macOS com as últimas atualizações, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Mantenho meu windows/linux/macOS com as últimas atualizações, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações
+    Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não tomo nenhuma dessas precauções
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam
+    Não clico em links aleatórios ou entro em sites desconhecidos.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Mantenho meu windows/linux/macOS com as últimas atualizações
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada., Deixo apenas meu antivirus tomar conta de meu computador
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    Não clico em links aleatórios ou entro em sites desconhecidos., Ignoro e-mails que caem diretamente na caixa de spam, Mantenho meu windows/linux/macOS com as últimas atualizações, Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive, Atualizo meu navegador sempre que uma atualização nova é lançada.
+    `;
+
+const replaceEverything = everything
+    .replaceAll(".,", "\n")
+    .replaceAll(",", "\n");
+const formatEverythingDown = replaceEverything
+    .replaceAll(
+        "Não clico em links aleatórios ou entro em sites desconhecidos.",
+        "Não clico em links aleatórios, ou entro em sites desconhecidos."
+    )
+    .replaceAll(
+        "Deixo apenas meu antivirus tomar conta de meu computador",
+        "Deixo apenas meu antivirus tomar conta de meu computador."
+    )
+    .replaceAll(
+        "Ignoro e-mails que caem diretamente na caixa de spam",
+        "Ignoro e-mails que caem diretamente na caixa de spam."
+    )
+    .replaceAll(
+        "Mantenho meu windows/linux/macOS com as últimas atualizações",
+        "Mantenho meu windows/linux/macOS com as últimas atualizações."
+    )
+    .replaceAll(
+        "Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive",
+        "Mantenho meus arquivos importantes em um backup na nuvem ou em um pendrive."
+    )
+    .replaceAll(
+        "Atualizo meu navegador sempre que uma atualização nova é lançada",
+        "Atualizo meu navegador sempre que uma atualização nova é lançada."
+    )
+    .replaceAll(
+        "Não clico em links aleatórios ou entro em sites desconhecidos",
+        "Não clico em links aleatórios, ou entro em sites desconhecidos."
+    )
+    .replaceAll("..", ".");
+console.log(formatEverythingDown);
